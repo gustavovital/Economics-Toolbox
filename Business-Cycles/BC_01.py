cf261cd42c636c617cec52bd3f6d7446bc4d1fc3
Gustavo Vital <gustavovital@id.uff.br>
2024-09-26 21:43:10 +0100

    Create BC_01.py
    Some analyses of BC

---
+++ Business-Cycles/BC_01.py
@@ -0,0 +1,167 @@
+# Packages to import
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime
+import wbgapi as wb
+import pandas_datareader.data as web
+
+# To change colors of lines
+cycler = plt.cycler(linestyle=['-', '-.', '--', ':'], 
+        color=['#377eb8', '#ff7f00', '#4daf4a', '#ff334f'])
+plt.rc('axes', prop_cycle=cycler)
+
+wb.series.info(q='GDP growth')
+
+# Get the data
+gdp_growth = wb.data.DataFrame('NY.GDP.MKTP.KD.ZG',
+            ['USA', 'ARG', 'GBR', 'GRC', 'JPN', 'BRA'], 
+            labels=True)
+gdp_growth
+
+# about the series
+wb.series.metadata.get('NY.GDP.MKTP.KD.ZG')
+
+# GDP growth rate
+# First we look at GDP growth.
+
+# Wrangling
+gdp_growth = gdp_growth.set_index('Country')
+gdp_growth.columns = gdp_growth.columns.str.replace('YR', '').astype(int)
+
+
+def plot_series(data, country, ylabel, 
+                txt_pos, ax, g_params,
+                b_params, t_params, ylim=15, baseline=0):
+    """
+    Plots a time series with recessions highlighted. 
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Data to plot
+    country : str
+        Name of the country to plot
+    ylabel : str
+        Label of the y-axis
+    txt_pos : float
+        Position of the recession labels
+    y_lim : float
+        Limit of the y-axis
+    ax : matplotlib.axes._subplots.AxesSubplot
+        Axes to plot on
+    g_params : dict
+        Parameters for the line
+    b_params : dict
+        Parameters for the recession highlights
+    t_params : dict
+        Parameters for the recession labels
+    baseline : float, optional
+        Dashed baseline on the plot, by default 0
+    
+    Returns
+    -------
+    ax : matplotlib.axes.Axes
+        Axes with the plot.
+    """
+
+    ax.plot(data.loc[country], label=country, **g_params)
+    
+    # Highlight recessions
+    ax.axvspan(1973, 1975, **b_params)
+    ax.axvspan(1990, 1992, **b_params)
+    ax.axvspan(2007, 2009, **b_params)
+    ax.axvspan(2019, 2021, **b_params)
+    if ylim != None:
+        ax.set_ylim([-ylim, ylim])
+    else:
+        ylim = ax.get_ylim()[1]
+    ax.text(1974, ylim + ylim*txt_pos,
+            'Oil Crisis\n(1974)', **t_params) 
+    ax.text(1991, ylim + ylim*txt_pos,
+            '1990s recession\n(1991)', **t_params) 
+    ax.text(2008, ylim + ylim*txt_pos,
+            'GFC\n(2008)', **t_params) 
+    ax.text(2020, ylim + ylim*txt_pos,
+            'Covid-19\n(2020)', **t_params)
+
+    # Add a baseline for reference
+    if baseline != None:
+        ax.axhline(y=baseline, 
+                   color='black', 
+                   linestyle='--')
+    ax.set_ylabel(ylabel)
+    ax.legend()
+    return ax
+
+# Define graphical parameters 
+g_params = {'alpha': 0.7}
+b_params = {'color':'grey', 'alpha': 0.2}
+t_params = {'color':'grey', 'fontsize': 9, 
+            'va':'center', 'ha':'center'}
+
+# plot recessions
+fig, ax = plt.subplots()
+
+country = 'Brazil'
+ylabel = 'GDP growth rate (%)'
+plot_series(gdp_growth, country, 
+            ylabel, 0.1, ax, 
+            g_params, b_params, t_params)
+plt.show()
+
+# UNENPLOYMENT
+start_date = datetime.datetime(1929, 1, 1)
+end_date = datetime.datetime(1942, 6, 1)
+
+unrate_history = web.DataReader('M0892AUSM156SNBR', 
+                    'fred', start_date,end_date)
+unrate_history.rename(columns={'M0892AUSM156SNBR': 'UNRATE'}, 
+                inplace=True)
+
+start_date = datetime.datetime(1948, 1, 1)
+end_date = datetime.datetime(2022, 12, 31)
+
+unrate = web.DataReader('UNRATE', 'fred', 
+                    start_date, end_date)  
+
+# We use the census bureau's estimate for the unemployment rate 
+# between 1942 and 1948
+years = [datetime.datetime(year, 6, 1) for year in range(1942, 1948)]
+unrate_census = [4.7, 1.9, 1.2, 1.9, 3.9, 3.9]
+
+unrate_census = {'DATE': years, 'UNRATE': unrate_census}
+unrate_census = pd.DataFrame(unrate_census)
+unrate_census.set_index('DATE', inplace=True)
+
+# Obtain the NBER-defined recession periods
+start_date = datetime.datetime(1929, 1, 1)
+end_date = datetime.datetime(2022, 12, 31)
+
+nber = web.DataReader('USREC', 'fred', start_date, end_date)
+
+fig, ax = plt.subplots()
+
+ax.plot(unrate_history, **g_params, 
+        color='#377eb8', 
+        linestyle='-', linewidth=2)
+ax.plot(unrate_census, **g_params, 
+        color='black', linestyle='--', 
+        label='Census estimates', linewidth=2)
+ax.plot(unrate, **g_params, color='#377eb8', 
+        linestyle='-', linewidth=2)
+
+# Draw gray boxes according to NBER recession indicators
+ax.fill_between(nber.index, 0, 1,
+                where=nber['USREC']==1, 
+                color='grey', edgecolor='none',
+                alpha=0.3, 
+                transform=ax.get_xaxis_transform(), 
+                label='NBER recession indicators')
+ax.set_ylim([0, ax.get_ylim()[1]])
+ax.legend(loc='upper center', 
+          bbox_to_anchor=(0.5, 1.1),
+          ncol=3, fancybox=True, shadow=True)
+ax.set_ylabel('unemployment rate (%)')
+
+plt.show()
